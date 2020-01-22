c28850cbd9fd385f0224c8d0d53506bd77eb7c04
shilpadeogade <shilpadeogade2203@gmail.com>
2020-01-22 16:45:21 +0530

    massage

---
+++ src/org/sele/DragDrop.java
@@ -0,0 +1,15 @@
+package org.sele;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DragDrop {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Atul Kojage\\eclipse-workspace\\SeleDrag7\\div7\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http:\\demo.guru.99.com\\test\\drag_drop.html");
+		
+		
+	}
+
+}
